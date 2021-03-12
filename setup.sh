9dc44bfd13aa5ba7ebc48a3610088f8c87e780ff
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-12 18:38:29 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt update -y && sudo apt upgrade -y
-sudo apt -y install screen nodejs unzip chromium-chromedriver tor
+sudo apt -y install screen nodejs unzip
+sudo apt -y install chromium-chromedriver
+sudo apt -y tor
 npm i puppeteer-core cheerio
