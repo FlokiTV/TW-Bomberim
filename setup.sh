6f00e3af4381631d55514e6fccd5997f3d83b03c
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-12 18:28:52 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt -y install screen unzip chromium-chromedriver tor
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt update -y & sudo apt upgrade -y
+sudo apt install -y nodejs unzip
+npm i puppeteer-core cheerio
