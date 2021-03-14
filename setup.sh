c4c600e453b6000cc436e36dc69d1b3e93d130ea
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-14 17:58:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y tor
 sudo apt -y install screen nodejs unzip
 #sudo apt -y install chromium-browser chromium-chromedriver
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y -f ./google-chrome-stable_current_amd64.deb
-npm i puppeteer-core cheerio
+sudo apt install -y tor
+npm i puppeteer-core cheerio socket.io express
