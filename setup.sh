ba7031c495c0c321b37ccb076adbb533a977364b
Floki <dawysonw@gmail.com>
2021-03-25 18:49:06 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt update -y && sudo apt upgrade -y
-sudo apt -y install screen nodejs unzip
+sudo apt -y install screen nodejs unzip nano curl zip unzip wget
 #sudo apt -y install chromium-browser chromium-chromedriver
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y -f ./google-chrome-stable_current_amd64.deb
